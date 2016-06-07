078332f193a6f226194f75b7be8209a3de38bf15
Brian Hartvigsen <brian.andrew@brianandjenny.com>
2016-06-07 04:51:04 -0600

    Metadata updates

--- setup.py
+++ setup.py
@@ -14,4 +14,19 @@ setup(name='hurricanedns',
       install_requires=['lxml', 'html5lib'],
       extras_require={'import': ["dnspython"]},
       py_modules=['HurricaneDNS'],
-      scripts=['hurricanedns'])
+      scripts=['hurricanedns'],
+      classifiers=[
+          "Development Status :: 5 - Production/Stable",
+          "Environment :: Console",
+          "Intended Audience :: Information Technology",
+          "Intended Audience :: System Administrators",
+          "License :: OSI Approved :: MIT License",
+          "Operating System :: OS Independent",
+          "Programming Language :: Python",
+          "Programming Language :: Python :: 2",
+          "Programming Language :: Python :: 2.7",
+          "Programming Language :: Python :: 3",
+          "Programming Language :: Python :: 3.5",
+          "Topic :: Internet :: Name Service (DNS)",
+          "Topic :: Utilities"
+      ])
